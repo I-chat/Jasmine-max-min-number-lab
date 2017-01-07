942cb748e8a4781e683433aba9e1437b6d714fc1
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-07 11:47:22 +0100

    Added some test cases

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -5,6 +5,12 @@ describe('Return the min and max number in the list in a new list follows `[min,
     expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
   });
 
+  it('should return [4, 15] for [7, 8, 10, 4, 15, 6]', function () {
+    expect(findMinMax([7, 8, 10, 4, 15, 6])).toEqual([4, 15]);
+  });
+
+
+
   it('should return [4, 6] for [6, 4]', function () {
     expect(findMinMax([6, 4])).toEqual([4, 6]);
   });
@@ -20,5 +26,9 @@ describe('Return the number in the list in a new list follows `[min]` when the n
   it('should return [4] for [4, 4, 4, 4]', function () {
     expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
   });
+
+  it('should return [72] for [72]', function () {
+      expect(findMinMax([72])).toEqual([72]);
+    });
 
 });
